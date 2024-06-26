b445767a5bacdacb2b1392a40b093593a2f6dad4
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2024-06-26 17:49:51 +0300

    add

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-const argsLength = process.argv.length;
 
-if (argsLength === 2) {
-  console.log("No argument");
-} else if (argsLength === 3) {
-  console.log("Argument found");
+if (process.argv.length <= 2) {
+  console.log('No argument');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
