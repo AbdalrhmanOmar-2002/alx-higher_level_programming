4a8dcdf863e0327a122e48eaa80189306a31be4a
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2024-06-26 17:53:52 +0300

    add

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
+
+// Script that prints "JavaScript is amazing".
+
 if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
+  console.log("No argument");
+}
+if (process.argv.length === 3) {
+  console.log("Argument found");
+}
+if (process.argv.length > 3) {
+  console.log("Arguments found");
 }
