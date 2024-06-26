17e1d05b68578f4a8a05d1b269d9f2b8ba7c4554
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2024-06-26 17:44:46 +0300

    add

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
+
+/**
+ * Script to print a message "JavaScript is amazing"
+ * This script creates a constant variable
+ * myVar with the value "JavaScript is amazing"
+ * and then prints it using console.log()
+ * Usage: ./0-javascript_is_amazing.js
+ * */
+
+const myVar = "JavaScript is amazing";
 console.log(myVar);
